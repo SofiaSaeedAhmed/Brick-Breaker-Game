a98436dedb3fe396f8c63fef79e5348164fe527c
SofiaSaeedAhmed <97895837+SofiaSaeedAhmed@users.noreply.github.com>
2023-11-26 16:17:22 +0800

    Update BlockSerializable.java
    Changes made:
    1- put comments

--- CourseworkGame-master/CourseworkGame-master/src/main/java/brickGame/BlockSerializable.java
+++ CourseworkGame-master/CourseworkGame-master/src/main/java/brickGame/BlockSerializable.java
@@ -3,10 +3,14 @@ package brickGame;
 import java.io.Serializable;
 
 public class BlockSerializable implements Serializable {
+    // represent position of the block in a 2D grid or array
     public final int row;
     public final int j;
+
+    // represents type of block
     public final int type;
 
+    // constructor
     public BlockSerializable(int row , int j , int type) {
         this.row = row;
         this.j = j;
