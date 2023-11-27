21758eef8a8058681eea639a74db294999fe0f15
SofiaSaeedAhmed <97895837+SofiaSaeedAhmed@users.noreply.github.com>
2023-11-27 18:49:44 +0800

    Add files via upload
    Separated the board method into a different file called Board.java to refactor

---
+++ CourseworkGame-master/CourseworkGame-master/src/main/java/brickGame/Board.java
@@ -0,0 +1,64 @@
+package brickGame;
+
+import javafx.scene.paint.Color;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Board {
+    private final ArrayList<Block> blocks;
+    private boolean isExistHeartBlock;
+    private final Color[] colors;
+
+    public Board(int level) {
+        blocks = new ArrayList<>();
+        isExistHeartBlock = false;
+        colors = new Color[]{
+                Color.MAGENTA,
+                Color.RED,
+                Color.GOLD,
+                Color.CORAL,
+                Color.AQUA,
+                Color.VIOLET,
+                Color.GREENYELLOW,
+                Color.ORANGE,
+                Color.PINK,
+                Color.SLATEGREY,
+                Color.YELLOW,
+                Color.TOMATO,
+                Color.TAN,
+        };
+
+        initBoard(level);
+    }
+
+    public void initBoard(int level) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < level + 1; j++) {
+                int r = new Random().nextInt(500);
+                if (r % 5 == 0) {
+                    continue;
+                }
+                int type;
+                if (r % 10 == 1) {
+                    type = Block.BLOCK_CHOCO;
+                } else if (r % 10 == 2) {
+                    if (!isExistHeartBlock) {
+                        type = Block.BLOCK_HEART;
+                        isExistHeartBlock = true;
+                    } else {
+                        type = Block.BLOCK_NORMAL;
+                    }
+                } else if (r % 10 == 3) {
+                    type = Block.BLOCK_STAR;
+                } else {
+                    type = Block.BLOCK_NORMAL;
+                }
+                blocks.add(new Block(j, i, colors[r % (colors.length)], type));
+            }
+        }
+    }
+
+    public ArrayList<Block> getBlocks() {
+        return blocks;
+    }
+}
