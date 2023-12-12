937f671d0d9c5e58d2a442ed31d2557baaacc58c
SofiaSaeedAhmed <97895837+SofiaSaeedAhmed@users.noreply.github.com>
2023-12-12 20:26:30 +0800

    Update Board.java
    Final code

--- CourseworkGame-master/CourseworkGame-master/src/main/java/brickGame/Board.java
+++ CourseworkGame-master/CourseworkGame-master/src/main/java/brickGame/Board.java
@@ -4,11 +4,23 @@ import javafx.scene.paint.Color;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * This class encapsulates the logic for initializing and maintaining the game board,
+ * including the types and colors of bricks present. Reference:
+ * <a href="https://github.com/kooitt/CourseworkGame/blob/master/src/main/java/brickGame/Main.java">Board.java Link</a>
+ */
+
 public class Board {
     private final ArrayList<Block> blocks;
     private boolean isExistHeartBlock;
+    private boolean isExistLongBlock;
     private final Color[] colors;
 
+    /**
+     * Initializes an empty list of blocks, sets flags for the existence of heart and longer blocks to false, and defines an array of colors.
+     * It then calls the initBoard method to populate the game board based on the specified level.
+     * @param level
+     */
     public Board(int level) {
         blocks = new ArrayList<>();
         isExistHeartBlock = false;
@@ -31,6 +43,10 @@ public class Board {
         initBoard(level);
     }
 
+    /**
+     * Generates a random number r to select a block type and set it on the game board.
+     * Heart and long can only be set once in a level.
+     */
     public void initBoard(int level) {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < level + 1; j++) {
@@ -50,7 +66,14 @@ public class Board {
                     }
                 } else if (r % 10 == 3) {
                     type = Block.BLOCK_STAR;
-                } else {
+                } else if (r % 10 == 4){
+                    if (!isExistLongBlock) {
+                        type = Block.BLOCK_LONGER;
+                        isExistLongBlock = true;
+                    } else {
+                        type = Block.BLOCK_NORMAL;
+                    }
+                }else {
                     type = Block.BLOCK_NORMAL;
                 }
                 blocks.add(new Block(j, i, colors[r % (colors.length)], type));
@@ -58,7 +81,13 @@ public class Board {
         }
     }
 
+    /**
+     * getter method to return blocks
+     * @return blocks
+     */
     public ArrayList<Block> getBlocks() {
         return blocks;
     }
 }
+
+
