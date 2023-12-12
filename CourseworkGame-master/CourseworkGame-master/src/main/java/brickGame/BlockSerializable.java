03a9c4cb0ab07564b80b5f7c48943143f9cb67ce
SofiaSaeedAhmed <97895837+SofiaSaeedAhmed@users.noreply.github.com>
2023-12-12 20:25:58 +0800

    Update BlockSerializable.java
    Final code

--- CourseworkGame-master/CourseworkGame-master/src/main/java/brickGame/BlockSerializable.java
+++ CourseworkGame-master/CourseworkGame-master/src/main/java/brickGame/BlockSerializable.java
@@ -2,18 +2,38 @@ package brickGame;
 
 import java.io.Serializable;
 
+/**
+ * This class is designed to represent the position and type of a block in a 2D grid or array. Reference:
+ *
+ * <a href="https://github.com/kooitt/CourseworkGame/blob/master/src/main/java/brickGame/BlockSerializable.java">BlockSerializable.java Link</a>
+ */
+
 public class BlockSerializable implements Serializable {
-    // represent position of the block in a 2D grid or array
+    /**
+     * represent position of the block in a 2D grid or array
+     */
     public final int row;
+
+    /**
+     * represent position of the block in a 2D grid or array
+     */
     public final int j;
 
-    // represents type of block
+    /**
+     * represents type of block
+     */
     public final int type;
 
-    // constructor
+    /**
+     *  initializes the position and type of a block in a 2D grid or array.
+     * @param row
+     * @param j
+     * @param type
+     */
     public BlockSerializable(int row , int j , int type) {
         this.row = row;
         this.j = j;
         this.type = type;
     }
 }
+
